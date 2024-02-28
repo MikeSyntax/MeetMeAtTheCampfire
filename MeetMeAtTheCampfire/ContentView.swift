462775e8269e5ae816978c72a3372ad60aee8020
MikeSyntax <autoglasgold@gmail.com>
2024-02-28 17:48:17 +0100

    InitialCommit

---
+++ MeetMeAtTheCampfire/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeetMeAtTheCampfire
+//
+//  Created by Mike Reichenbach on 28.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hallo")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeetMeAtTheCampfire/MeetMeAtTheCampfireApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetMeAtTheCampfireApp.swift
+//  MeetMeAtTheCampfire
+//
+//  Created by Mike Reichenbach on 28.02.24.
+//
+
+import SwiftUI
+
+@main
+struct MeetMeAtTheCampfireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
