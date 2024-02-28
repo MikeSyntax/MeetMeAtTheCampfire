1345e74a0dfebdc8a9679c9404da34298f63a9fc
MikeSyntax <autoglasgold@gmail.com>
2024-02-28 18:07:15 +0100

    Commit

--- MeetMeAtTheCampfire/ContentView.swift
+++ MeetMeAtTheCampfire/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hallo")
+            Text("Huhu")
         }
         .padding()
     }
