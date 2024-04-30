11b20ff8496fe02997163b96fcfdb4fa7233c613
MikeSyntax <autoglasgold@gmail.com>
2024-04-30 13:12:39 +0200

    ProgressView für Übersetzer

--- MeetMeAtTheCampfire/ÜbersetzerScreen/LanguageScreenView.swift
+++ MeetMeAtTheCampfire/ÜbersetzerScreen/LanguageScreenView.swift
@@ -12,6 +12,7 @@ struct LanguageScreenView: View {
     @ObservedObject var languageVm: LanguageScreenViewModel
     @State private var selectionLanguage: Language = Language(code: "af", name: "Afrikaans")
     @State private var selectionSourceLanguage: Language = Language(code: "de", name: "Deutsch")
+    @State private var isLoading: Bool = false
     
     var body: some View {
         NavigationStack {
@@ -101,30 +102,76 @@ struct LanguageScreenView: View {
                         )
                     }
                     Divider()
-                    VStack(alignment: .leading){
-                        Text("Ausgabe der Übersetzung".uppercased())
-                            .underline()
-                            .font(.system(size: 14, design: .monospaced))
+                    if !languageVm.textToTranslate.isEmpty {
                         ZStack{
-                            if languageVm.translatedText.isEmpty {
-                                Text("Übersetzung")
-                                    .opacity(0.4)
+                            if !isLoading || !languageVm.translatedText.isEmpty {
+                                VStack(alignment: .leading){
+                                    Text("Ausgabe der Übersetzung".uppercased())
+                                        .underline()
+                                        .font(.system(size: 14, design: .monospaced))
+                                    ZStack{
+                                        if languageVm.translatedText.isEmpty {
+                                            Text("Übersetzung")
+                                                .opacity(0.4)
+                                        }
+                                        Text(languageVm.translatedText)
+                                            .font(.system(size: 17).bold())
+                                            .textFieldStyle(.roundedBorder)
+                                            .autocorrectionDisabled()
+                                            .textInputAutocapitalization(.never)
+                                            .padding(3)
+                                            .frame(minWidth: 310, minHeight: 150)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .stroke(Color.red, lineWidth: 2)
+                                            )
+                                    }
+                                }
                             }
-                            Text(languageVm.translatedText)
-                                .font(.system(size: 17).bold())
-                                .textFieldStyle(.roundedBorder)
-                                .autocorrectionDisabled()
-                                .textInputAutocapitalization(.never)
-                                .padding(3)
-                                .frame(minWidth: 310, minHeight: 150)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.red, lineWidth: 2)
-                                )
+                            if isLoading && languageVm.translatedText.isEmpty {
+                                VStack(alignment: .leading){
+                                    Text("Ausgabe der Übersetzung".uppercased())
+                                        .underline()
+                                        .font(.system(size: 14, design: .monospaced))
+                                    ProgressView()
+                                        .progressViewStyle(CircularProgressViewStyle())
+                                        .scaleEffect(3)
+                                        .frame(minWidth: 310, minHeight: 150)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(Color.red, lineWidth: 2)
+                                        )
+                                }
+                            }
+                        }
+                        .onAppear{
+                            isTranslationIsLoading()
+                        }
+                    } else {
+                        VStack(alignment: .leading){
+                            Text("Ausgabe der Übersetzung".uppercased())
+                                .underline()
+                                .font(.system(size: 14, design: .monospaced))
+                            ZStack{
+                                if languageVm.translatedText.isEmpty {
+                                    Text("Übersetzung")
+                                        .opacity(0.4)
+                                }
+                                Text(languageVm.translatedText)
+                                    .font(.system(size: 17).bold())
+                                    .textFieldStyle(.roundedBorder)
+                                    .autocorrectionDisabled()
+                                    .textInputAutocapitalization(.never)
+                                    .padding(3)
+                                    .frame(minWidth: 310, minHeight: 150)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.red, lineWidth: 2)
+                                    )
+                            }
                         }
                     }
                 }
-//                .padding(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 40))
                 Divider()
             }
             .scrollContentBackground(.hidden)
@@ -151,6 +198,12 @@ struct LanguageScreenView: View {
         }
         .background(Color(UIColor.systemBackground))
     }
+    func isTranslationIsLoading(){
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1000.0){
+            isLoading = false
+        }
+    }
 }
 
 #Preview {
@@ -162,6 +215,7 @@ struct LanguageScreenView: View {
 
 
 
+//
 //import SwiftUI
 //
 //struct LanguageScreenView: View {
@@ -182,16 +236,22 @@ struct LanguageScreenView: View {
 //                        VStack(alignment: .trailing){
 //                            ZStack{
 //                                Text(languageVm.emptyField)
-//                                    .frame(minWidth: 310, minHeight: 70)
-//                                    .border(Color.white, width: 2)
-//                                    .background(.white.opacity(0.7))
 //                                Picker("Übersetze nach", selection: $selectionLanguage) {
 //                                    ForEach(languageVm.languages, id: \.code) { language in
 //                                        Text(language.name)
 //                                            .tag(language)
 //                                    }
 //                                }
+//                                .font(.system(size: 17).bold())
+//                                .textFieldStyle(.roundedBorder)
+//                                .padding(3)
+//                                .background(Color.white.opacity(0.8))
+//                                .cornerRadius(10)
 //                                .frame(minWidth: 310, minHeight: 70)
+//                                .overlay(
+//                                    RoundedRectangle(cornerRadius: 10)
+//                                        .stroke(Color.cyan, lineWidth: 2)
+//                                )
 //                                .pickerStyle(.menu)
 //                                .onChange(of: selectionLanguage) { languageVm.languageChoice = selectionLanguage }
 //                            }
@@ -205,23 +265,19 @@ struct LanguageScreenView: View {
 //                        VStack(alignment: .trailing){
 //                            ZStack{
 //                                if languageVm.textToTranslate.isEmpty {
-//                                    Text("Text eingeben")
-//                                        .opacity(0.6)
+//                                    Text("Hier Text eingeben")
+//                                        .opacity(0.4)
 //                                }
-//                                TextEditor(text: $languageVm.textToTranslate/*, axis: .vertical*/)
-//                                    .onChange(of: languageVm.textToTranslate) { newValue, _ in
-//                                        if newValue.count > 300 {
-//                                            languageVm.textToTranslate = String(newValue.prefix(300))
-//                                        }
-//                                    }
+//                                TextEditor(text: $languageVm.textToTranslate)
 //                                    .font(.system(size: 17).bold())
-//                                    .padding(3)
-//                                    .background(.cyan.opacity(0.4))
-//                                    .cornerRadius(6)
+//                                    .textFieldStyle(.roundedBorder)
 //                                    .autocorrectionDisabled()
-//                                    .lineLimit(1...5)
-//                                    .frame(minWidth: 310, minHeight: 150)
-//                                    .border(Color.white, width: 2)
+//                                    .padding(3)
+//                                    .frame(width: 310, height: 150)
+//                                    .overlay(
+//                                        RoundedRectangle(cornerRadius: 10)
+//                                            .stroke(Color.cyan, lineWidth: 2)
+//                                    )
 //                            }
 //                            .keyboardType(.default)
 //                            .submitLabel(.done)
@@ -235,19 +291,25 @@ struct LanguageScreenView: View {
 //                        HStack{
 //                            Text("\(selectionSourceLanguage.name)")
 //                                .font(.system(size: 14))
-//                            Spacer()
+//                                .padding(4)
 //                            ButtonTextAction(iconName: "network", text: "Übersetzen"){
 //                                languageVm.translateLanguage()
 //                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
 //                            }
-//                            Spacer()
 //                            Text("\(selectionLanguage.name)")
 //                                .font(.system(size: 14))
+//                                .padding(4)
 //                        }
-//                        .padding(9)
-//                        .frame(maxWidth: .infinity, minHeight: 70, alignment: .center)
-//                        .border(Color.white, width: 2)
-//                        .background(.cyan.opacity(0.4))
+//                        .font(.system(size: 17).bold())
+//                        .textFieldStyle(.roundedBorder)
+//                        .autocorrectionDisabled()
+//                        .textInputAutocapitalization(.never)
+//                        .padding(3)
+//                        .frame(minWidth: 310, minHeight: 70)
+//                        .overlay(
+//                            RoundedRectangle(cornerRadius: 10)
+//                                .stroke(Color.cyan, lineWidth: 2)
+//                        )
 //                    }
 //                    Divider()
 //                    VStack(alignment: .leading){
@@ -257,20 +319,22 @@ struct LanguageScreenView: View {
 //                        ZStack{
 //                            if languageVm.translatedText.isEmpty {
 //                                Text("Übersetzung")
-//                                    .opacity(0.6)
+//                                    .opacity(0.4)
 //                            }
 //                            Text(languageVm.translatedText)
 //                                .font(.system(size: 17).bold())
+//                                .textFieldStyle(.roundedBorder)
+//                                .autocorrectionDisabled()
+//                                .textInputAutocapitalization(.never)
 //                                .padding(3)
 //                                .frame(minWidth: 310, minHeight: 150)
-//                                .border(Color.white, width: 2)
-//                                .background(.cyan.opacity(0.4))
-//                                
+//                                .overlay(
+//                                    RoundedRectangle(cornerRadius: 10)
+//                                        .stroke(Color.red, lineWidth: 2)
+//                                )
 //                        }
 //                    }
 //                }
-//                .padding(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 40))
-//                Spacer()
 //                Divider()
 //            }
 //            .scrollContentBackground(.hidden)
