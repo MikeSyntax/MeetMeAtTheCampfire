9daead32d24e823e61b3219c894fc9b57d9157c2
MikeSyntax <autoglasgold@gmail.com>
2024-05-05 17:48:22 +0200

    Kleine Änderungen an der UI

--- MeetMeAtTheCampfire/APIKey.swift
+++
@@ -1,8 +0,0 @@
-//
-//  APIKey.swift
-//  MeetMeAtTheCampfire
-//
-//  Created by Mike Reichenbach on 05.05.24.
-//
-
-import Foundation
--- MeetMeAtTheCampfire/Adress.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Adress.swift
-//  MeetMeAtTheCampfire
-//
-//  Created by Mike Reichenbach on 05.05.24.
-//
-
-import Foundation
