c67c4775773d1249b7f2407bf15d53862e4b2af7
MikeSyntax <autoglasgold@gmail.com>
2024-06-30 15:02:59 +0200

    added startscreen progressiveView

--- MeetMeAtTheCampfire/MeetMeAtTheCampfire.swift
+++ MeetMeAtTheCampfire/MeetMeAtTheCampfire.swift
@@ -12,23 +12,43 @@ import SwiftData
 @main
 struct MeetMeAtTheCampfireApp: App {
     @StateObject var authVm = AuthViewModel()
+    @State private var showProgressiveView: Bool = true
     
-    init(){
+    init() {
         FirebaseConfiguration.shared.setLoggerLevel(.min)
         FirebaseApp.configure()
     }
     
     var body: some Scene {
         WindowGroup {
-            if authVm.userLoggedIn {
-                MainScreenView(authVm: authVm)
-                    .environmentObject(authVm)
-            } else {
-                LoginView()
-                    .environmentObject(authVm)
+            Group {
+                if showProgressiveView {
+                    Text("Loading Campfire")
+                        .padding()
+                    ProgressView()
+                        .progressViewStyle(DefaultProgressViewStyle())
+                        .scaleEffect(3)
+                } else {
+                    if authVm.userLoggedIn {
+                        MainScreenView(authVm: authVm)
+                            .environmentObject(authVm)
+                    } else {
+                        LoginView()
+                            .environmentObject(authVm)
+                    }
+                }
+            }
+            .environment(\.locale, .init(identifier: "de"))
+            .modelContainer(for: [LogBookAtivity.self])
+            .onAppear{
+                isStartScreenLoading()
             }
         }
-        .environment(\.locale, .init(identifier: "de"))
-        .modelContainer(for: [LogBookAtivity.self])
+    }
+    
+    func isStartScreenLoading() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            showProgressiveView = false
+        }
     }
 }
